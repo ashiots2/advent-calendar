7b0f767233bad43b515eadf511f7965bf98ab41f
Savvas Ashiotis <ashiots2@gmail.com>
2024-11-25 22:34:43 +0200

    add reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,18 @@ import {useState} from 'react'
 import './App.css'
 import {CalendarDateProperties} from "./types/CalendarDateProperties.ts";
 import {Calendar} from "./components/Calendar.tsx";
+import {initialData} from "./data/InitialData.ts";
 
 function App() {
-    const [calendarDates, setCalendarDates] = useState<CalendarDateProperties[]>([
-        {date: 1, isOpen: false, imagePath: ''},
-        {date: 2, isOpen: false, imagePath: ''},
-        {date: 3, isOpen: false, imagePath: ''},
-        {date: 4, isOpen: false, imagePath: ''},
-        {date: 5, isOpen: false, imagePath: ''},
-        {date: 6, isOpen: false, imagePath: ''},
-        {date: 7, isOpen: false, imagePath: ''},
-        {date: 8, isOpen: false, imagePath: ''},
-        {date: 9, isOpen: false, imagePath: ''},
-        {date: 11, isOpen: false, imagePath: ''},
-        {date: 10, isOpen: false, imagePath: ''},
-        {date: 12, isOpen: false, imagePath: ''},
-        {date: 13, isOpen: false, imagePath: ''},
-        {date: 14, isOpen: false, imagePath: ''},
-        {date: 15, isOpen: false, imagePath: ''},
-        {date: 16, isOpen: false, imagePath: ''},
-        {date: 17, isOpen: false, imagePath: ''},
-        {date: 18, isOpen: false, imagePath: ''},
-        {date: 19, isOpen: false, imagePath: ''},
-        {date: 20, isOpen: false, imagePath: ''},
-        {date: 21, isOpen: false, imagePath: ''},
-        {date: 22, isOpen: false, imagePath: ''},
-        {date: 23, isOpen: false, imagePath: ''},
-        {date: 24, isOpen: false, imagePath: ''},
-    ])
+    const [calendarDates, setCalendarDates] = useState<CalendarDateProperties[]>(initialData);
+
+    const resetCalendarDates = () => {
+        setCalendarDates(prevCalendarDates =>
+            prevCalendarDates.map(item =>
+                Object.assign(item, {isOpen: false})
+            )
+        );
+    };
 
     const toggleDate = (date: number) => {
         setCalendarDates(prevCalendarDates =>
@@ -46,7 +30,7 @@ function App() {
         <>
             <h2>BarbieTheNinja's Advent Calendar</h2>
             <Calendar calendarDates={calendarDates} toggleDate={toggleDate}>
-                <div className={"calendar-title"}>
+                <div onClick={resetCalendarDates} className={"calendar-title"}>
                     <span className={"calendar-title__highlighted calendar-title__larger font-glacial-indifference"}>Tiny builds</span>
                     <span className={"calendar-title__highlighted font-amsterdam"}>of</span>
                     <span className={"calendar-title__alternative-highlighted font-amsterdam"}>Christmas</span>
