10c5d3e032e5e4931b191e9232d9cdca2ed89f48
Savvas Ashiotis <ashiots2@gmail.com>
2024-11-25 23:45:42 +0200

    decorations

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,6 +2,7 @@ import {OpenableDate} from "./OpenableDate.tsx";
 import {CalendarDateProperties} from "../types/CalendarDateProperties.ts";
 import "./Calendar.css"
 import {ReactNode} from "react";
+import xmasTree from '../assets/xmas_tree.svg'
 
 export const Calendar = ({children, calendarDates, toggleDate}: {
     calendarDates: CalendarDateProperties[],
@@ -10,6 +11,9 @@ export const Calendar = ({children, calendarDates, toggleDate}: {
 }) => {
     return (
         <div className="calendar">
+            <div className={"xmas-tree-card"}>
+                <img className={"xmas-tree-img"} src={xmasTree} alt={"xmas-tree"}/>
+            </div>
             {
                 calendarDates.map((x) => (
                     <OpenableDate key={"openable-date-" + x.date} dateNumber={x.date} isOpen={x.isOpen}
